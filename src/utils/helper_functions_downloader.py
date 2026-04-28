c71c8e7585479575738a0b8ced887b9207e8cfa6
Sasi Kumar K <sasikumar.karikuti@v-optimaise.com>
2026-04-28 17:57:41 +0530

    code rebased

--- src/utils/helper_functions_downloader.py
+++ src/utils/helper_functions_downloader.py
@@ -65,18 +65,16 @@ def extract_datetime_from_filename(filename: str) -> datetime:
         raise ValueError(f"Filename does not match the expected datetime format: {stem}") from e
 
 def setup_browser_driver():
-    """
-    Set up and return a Selenium Chrome WebDriver with predefined options.
-
-    Returns:
-        webdriver.Chrome: Configured Chrome WebDriver instance.
-    """
     chrome_options = Options()
+
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
-    service = Service(ChromeDriverManager().install())
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+    service = Service("/usr/bin/chromedriver")
+
     return webdriver.Chrome(service=service, options=chrome_options)
 
 def login_eml(driver, wait, LOGIN_URL, USER, PASSWD):
